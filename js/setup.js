7ed5055ce140b35bc26a95e76e1dfcc0fcd9f7f3
popwag <wagnerpoppy@gmail.com>
2025-04-03 15:53:18 -0400

    Add files via upload

---
+++ js/setup.js
@@ -0,0 +1,8 @@
+var map = L.map('map', {
+  center: [44.4620, -93.1525],
+  zoom: 16,
+});
+
+var Satellite = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
